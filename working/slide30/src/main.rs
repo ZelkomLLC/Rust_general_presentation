12e79ed3b859d64641b00ea26ff272a76e016664
KortesKnight <alekszelik@gmail.com>
2024-09-14 00:48:14 +0400

    Добавил слайд 30

--- working/slide30/src/main.rs
+++ working/slide30/src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let s1 = String::from("Привет");
-    let len = calculate_length(&s1); // передача по ссылке (заимствование)
+    let mut x = 5; // переменная изменяема
+    println!("x: {}", x);
 
-    println!("Длина строки '{}': {}", s1, len); // s1 доступен, т.к. заимствован
-}
-
-fn calculate_length(s: &String) -> usize {
-    s.len() // доступ к данным через ссылку
+    x = 10; // теперь можно изменить значение
+    println!("x: {}", x);
 }
