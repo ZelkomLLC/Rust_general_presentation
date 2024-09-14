acf3c4602e926641ff7420ade40b74b837aa6d75
KortesKnight <alekszelik@gmail.com>
2024-09-14 17:18:49 +0400

    Добавил слайд 78

---
+++ working/slide78/src/main.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+struct ApiResponse {
+    id: u32,
+    title: String,
+    body: String,
+}
+
+async fn fetch_data() -> Result<ApiResponse, reqwest::Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts/1";
+
+    let response = match reqwest::get(url).await {
+        Ok(res) => res,
+        Err(err) => return Err(err),
+    };
+
+    response.json::<ApiResponse>().await
+}
+
+#[tokio::main]
+async fn main() {
+    match fetch_data().await {
+        Ok(data) => println!("Полученные данные: {:#?}", data),
+        Err(e) => println!("Ошибка при запросе: {}", e),
+    }
+}
